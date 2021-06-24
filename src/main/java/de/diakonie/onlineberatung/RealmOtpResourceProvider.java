97c91072ef4f109b3d0bec7e34e0d721ad6122f9
Sergio Vidangos <sergio.vidangos@virtual-identity.com>
2021-06-24 19:48:13 +0200

    feat: add authentication; improve validation before setting up otp for user

--- src/main/java/de/diakonie/onlineberatung/RealmOtpResourceProvider.java
+++ src/main/java/de/diakonie/onlineberatung/RealmOtpResourceProvider.java
@@ -2,11 +2,11 @@ package de.diakonie.onlineberatung;
 
 import de.diakonie.onlineberatung.keycloak_otp_config_spi.keycloakextension.generated.web.model.OtpInfoDTO;
 import de.diakonie.onlineberatung.keycloak_otp_config_spi.keycloakextension.generated.web.model.OtpSetupDTO;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -14,15 +14,12 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-import org.keycloak.authentication.authenticators.challenge.BasicAuthOTPAuthenticatorFactory;
-import org.keycloak.authentication.authenticators.directgrant.ValidateOTP;
-import org.keycloak.credential.CredentialModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
 import org.keycloak.models.credential.OTPCredentialModel;
+import org.keycloak.models.utils.CredentialValidation;
 import org.keycloak.models.utils.HmacOTP;
-import org.keycloak.protocol.oidc.TokenManager;
 import org.keycloak.services.managers.AppAuthManager;
 import org.keycloak.services.managers.AuthenticationManager;
 import org.keycloak.services.resource.RealmResourceProvider;
@@ -31,39 +28,22 @@ import org.keycloak.utils.TotpUtils;
 
 public class RealmOtpResourceProvider implements RealmResourceProvider {
 
+  public static final int KEY_LENGTH = 32;
+  public static final String ROLE_REQUIRED = "technical";
+
   private final KeycloakSession session;
 
-  /*private final AuthenticationManager.AuthResult authResult;
-
-  private final AppAuthManager authManager;*/
-
-  private TokenManager tokenManager;
-
   public RealmOtpResourceProvider(KeycloakSession session) {
-    //this.authManager = new AppAuthManager();
     this.session = session;
-    System.out.println("Realm: "+session.getContext().getRealm());
-    System.out.println("Realm this: "+this.session.getContext().getRealm());
-    /*System.out.println(session);
-
-    System.out.println(session.getContext().getRequestHeaders());
-    String tokenString = authManager.extractAuthorizationHeaderToken(session.getContext().getRequestHeaders());
-    System.out.println("tokenString: "+tokenString);
-    System.out.println("URI: "+session.getContext().getUri());
-    System.out.println("Connection: "+ session.getContext().getConnection());
-    this.authResult = authManager
-        .authenticateBearerToken(tokenString, session, session.getContext().getRealm(), session.getContext().getUri(),
-            session.getContext().getConnection(), session.getContext().getRequestHeaders());
-    System.out.println(authResult);*/
   }
 
   @GET
   @Path("fetch-otp-setup-info/{username}")
   @Produces({MediaType.APPLICATION_JSON})
   public Response getOtpSetupInfo(@PathParam("username") final String username) {
+    verifyAuthentication();
 
     final RealmModel realm = this.session.getContext().getRealm();
-    System.out.println(realm);
     final UserModel user = this.session.users().getUserByUsername(username, realm);
 
     var otpInfoDTO = new OtpInfoDTO();
@@ -71,7 +51,7 @@ public class RealmOtpResourceProvider implements RealmResourceProvider {
         this.session.userCredentialManager().isConfiguredFor(realm, user, OTPCredentialModel.TYPE));
 
     if (Boolean.FALSE.equals(otpInfoDTO.getOtpSetup())) {
-      String otpSecret = HmacOTP.generateSecret(20);
+      String otpSecret = HmacOTP.generateSecret(KEY_LENGTH);
       otpInfoDTO.setOtpSecret(otpSecret);
       otpInfoDTO.setOtpSecretQrCode(TotpUtils.qrCode(otpSecret, realm, user));
     }
@@ -81,21 +61,26 @@ public class RealmOtpResourceProvider implements RealmResourceProvider {
 
   @PUT
   @Path("setup-otp/{username}")
-  @Consumes("application/json")
+  @Consumes({MediaType.APPLICATION_JSON})
   public Response setupOtp(@PathParam("username") final String username, final OtpSetupDTO dto) {
+    verifyAuthentication();
+
     final RealmModel realm = this.session.getContext().getRealm();
     final UserModel user = this.session.users().getUserByUsername(username, realm);
+
     if (!this.session.userCredentialManager()
         .isConfiguredFor(realm, user, OTPCredentialModel.TYPE)) {
+
       final var otpCredentialModel = OTPCredentialModel
           .createFromPolicy(realm, dto.getSecret());
-      boolean validCode = CredentialHelper
-          .createOTPCredential(this.session, realm, user, dto.getInitialCode(), otpCredentialModel);
 
-      if (Boolean.FALSE.equals(validCode)){
-        deleteAllOtpCredentials(realm, user);
-        return Response.status(Status.UNAUTHORIZED).build();
+      if (!CredentialValidation.validOTP(dto.getInitialCode(), otpCredentialModel,
+          realm.getOTPPolicy().getLookAheadWindow())) {
+        return Response.status(Status.UNAUTHORIZED).entity("Invalid otp code").build();
       }
+
+      CredentialHelper
+          .createOTPCredential(this.session, realm, user, dto.getInitialCode(), otpCredentialModel);
 
       return Response.status(Status.CREATED).entity("OTP credential created").build();
     }
@@ -106,63 +91,34 @@ public class RealmOtpResourceProvider implements RealmResourceProvider {
   @DELETE
   @Path("delete-otp/{username}")
   public Response deleteOtp(@PathParam("username") final String username) {
+    verifyAuthentication();
+
     final RealmModel realm = this.session.getContext().getRealm();
     final UserModel user = this.session.users().getUserByUsername(username, realm);
     deleteAllOtpCredentials(realm, user);
 
-    return Response.ok().build();
+    return Response.ok("success").build();
   }
 
   private void deleteAllOtpCredentials(RealmModel realm, UserModel user) {
     this.session.userCredentialManager().
         getStoredCredentialsByType(realm, user, OTPCredentialModel.TYPE).
-        stream().map(CredentialModel::getId).
-        forEach(id -> CredentialHelper.deleteOTPCredential(this.session, realm, user, id));
+        forEach(credentialModel -> CredentialHelper
+            .deleteOTPCredential(this.session, realm, user, credentialModel.getId()));
   }
 
-  private boolean isAuthenticatedRequest() {
-    /*System.out.println(httpHeaders);
-    String tokenString = authManager.extractAuthorizationHeaderToken(httpHeaders);
-    MultivaluedMap<String, String> queryParameters = session.getContext().getUri()
-        .getQueryParameters();
-    if (tokenString == null && queryParameters.containsKey("access_token")) {
-      tokenString = queryParameters.getFirst("access_token");
-    }
-    if (tokenString == null) {
-      return false;
-    }
-    AccessToken token;
-    try {
-      JWSInput input = new JWSInput(tokenString);
-      token = input.readJsonContent(AccessToken.class);
-    } catch (JWSInputException e) {
-      return false;
-    }
-    String realmName = token.getIssuer().substring(token.getIssuer().lastIndexOf('/') + 1);
-    RealmManager realmManager = new RealmManager(session);
-    RealmModel realm = realmManager.getRealmByName(realmName);
-    if (realm == null) {
-      return false;
-    }
-    session.getContext().setRealm(realm);
-    AuthenticationManager.AuthResult authResult = authManager
-        .authenticateBearerToken(tokenString, session, realm, session.getContext().getUri(),
-            clientConnection, httpHeaders);
-    if (authResult == null) {
-      return false;
-    }
+  private void verifyAuthentication() {
+    final AuthenticationManager.AuthResult auth = new AppAuthManager()
+        .authenticateBearerToken(session);
 
-    ClientModel client = realm.getClientByClientId(token.getIssuedFor());
-    if (client == null) {
-      return false;
+    if (auth == null) {
+      throw new NotAuthorizedException("Bearer");
+    } else if (auth.getToken().getRealmAccess() == null || !auth.getToken().getRealmAccess()
+        .isUserInRole(
+            ROLE_REQUIRED)) {
+      throw new ForbiddenException("Does not have required role");
     }
-
-    AdminAuth adminAuth = new AdminAuth(realm, authResult.getToken(), authResult.getUser(), client);
-    adminAuth.getUser().getRealmRoleMappings().forEach(System.out::println);
-    return false;*/
-    return true;
   }
-
 
   @Override
   public Object getResource() {
--- src/main/java/de/diakonie/onlineberatung/authenticator/OtpParameterAuthenticator.java
+++ src/main/java/de/diakonie/onlineberatung/authenticator/OtpParameterAuthenticator.java
@@ -1,34 +1,45 @@
 package de.diakonie.onlineberatung.authenticator;
 
+import java.util.Collections;
+import java.util.List;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.AuthenticationFlowError;
-import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.authenticators.directgrant.AbstractDirectGrantAuthenticator;
+import org.keycloak.models.AuthenticationExecutionModel;
+import org.keycloak.models.AuthenticationExecutionModel.Requirement;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
+import org.keycloak.provider.ProviderConfigProperty;
 
-public class OtpParameterAuthenticator implements Authenticator {
+public class OtpParameterAuthenticator extends AbstractDirectGrantAuthenticator {
+
+  public static final String ID = "otp-parameter-authenticator";
+  AuthenticationExecutionModel.Requirement[] REQUIREMENT_CHOICES = {
+      AuthenticationExecutionModel.Requirement.REQUIRED};
 
   @Override
-  public void authenticate(AuthenticationFlowContext authenticationFlowContext) {
-    MultivaluedMap<String, String> inputData = authenticationFlowContext.getHttpRequest().getDecodedFormParameters();
+  public String getId() {
+    return ID;
+  }
+
+  @Override
+  public void authenticate(AuthenticationFlowContext context) {
+    MultivaluedMap<String, String> inputData = context.getHttpRequest().getDecodedFormParameters();
     String otp = inputData.getFirst("otp");
     otp = (otp == null) ? inputData.getFirst("totp") : otp;
 
     if (otp == null) {
-      authenticationFlowContext.failure(
-          AuthenticationFlowError.INVALID_CREDENTIALS, Response.status(Status.BAD_REQUEST).entity("{\"otpMissing\": true}").type("application/json").build());
+      Response challengeResponse = errorResponse(Status.BAD_REQUEST.getStatusCode(),
+          "invalid_grant", "Missing totp");
+      context.failure(
+          AuthenticationFlowError.INVALID_CREDENTIALS, challengeResponse);
       return;
     }
-    authenticationFlowContext.success();
-  }
-
-  @Override
-  public void action(AuthenticationFlowContext authenticationFlowContext) {
-    // Do nothing because it is not needed
+    context.success();
   }
 
   @Override
@@ -49,7 +60,37 @@ public class OtpParameterAuthenticator implements Authenticator {
   }
 
   @Override
-  public void close() {
-    // Do nothing because it is not needed
+  public String getDisplayType() {
+    return "OTP parameter validator";
+  }
+
+  @Override
+  public String getReferenceCategory() {
+    return null;
+  }
+
+  @Override
+  public boolean isConfigurable() {
+    return false;
+  }
+
+  @Override
+  public Requirement[] getRequirementChoices() {
+    return REQUIREMENT_CHOICES;
+  }
+
+  @Override
+  public boolean isUserSetupAllowed() {
+    return false;
+  }
+
+  @Override
+  public String getHelpText() {
+    return "Validates if the otp parameter is set when OTP is enabled for the user";
+  }
+
+  @Override
+  public List<ProviderConfigProperty> getConfigProperties() {
+    return Collections.emptyList();
   }
 }
